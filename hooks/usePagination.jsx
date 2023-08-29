ce369e2871fc449e7249ba3c735267f0a081c5aa
Ismail <hossainmdismail9010@gmail.com>
2023-08-29 16:32:10 +0600

    added usePagination hooks

---
+++ hooks/usePagination.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+function usePagination(data, itemsPerPage) {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const currentData = data.slice(startIndex, endIndex);
+
+  const goToPage = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return {
+    currentPage,
+    currentData,
+    totalPages,
+    goToPage,
+    nextPage,
+    prevPage,
+  };
+}
+
+export default usePagination;
